ec7d6adb142d8f9c3bef377a5365db9a33429d99
TANAY PANT <tanaypant71@gmail.com>
2023-08-12 21:55:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+//comment
+// console.log('Hello World!!!');
+//==============================================
+// let  name = 'Tanay';
+// console.log(name);
+// let firstName = 'Tanay';
+//==============================================
+// const interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+//==============================================
+// let name = 'Tanay';
+// let age = 21;
+// let isApproved = true;
+// let firstName = undefined;
+// let lastName = null;
+// console.log(lastName);
+//=============================================
+
+/* Javascript is a dynamic language */
+
+// let person = {
+//     name: 'Tanay',
+//     age: 21
+// };
+// //Dot Notation
+// person.name = 'John';
+
+// //Bracket Notation
+// person['name'] = 'Man';
+
+// console.log(person.name);
+
+
+//Arrays
+// let selectedColors = ['red','blue'];
+// selectedColors[2] = 1;
+// console.log(selectedColors.length);
+
+
+//Functions
+// function greet(name,number)
+// {
+//     //body
+//     console.log('HI '+name+' '+number);
+// }
+
+// greet('Tanay',1);
+// greet('Anay');
+
+//Loops
+var arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+// for(var i = 0;i<arr.length;i++)
+// {
+//     console.log(arr[i]);
+// }
+
+// arr.forEach(function(element)
+// {
+//     console.log(element);
+// })
+
